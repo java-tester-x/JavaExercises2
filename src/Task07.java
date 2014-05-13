0578586623781a66db98ec7f6822dc3430d74f9a
java-tester-x <iohonko@gmail.com>
2014-05-13 20:29:01 +0300

    Finished Task07 and start works on Task08
    Task08: Find a ten-digit number that all digits are different by rules:
    1) the number, composed of the first 2 digits is divisible by 2;
    2) the number, composed of the first 3 digits is divisible by 3;
    3) the number, composed of the first 4 digits is divisible by 4;
    ...
    x) and the number itself is divided by 10.

--- src/Task07.java
+++ src/Task07.java
@@ -20,52 +20,126 @@ public class Task07 extends AbstractTask {
 
     private int[][] matrix;
 
-    public static void main()
+    public static void main(String[] args)
+    {
+        Task07 t = new Task07();
+        t.run();
+    }
+
+    private void fillMatrixFromKeyboard()
+    {
+        Scanner in = new Scanner(System.in);
+
+        int rowCount   = 0;
+        int columCount = 0;
+        do {
+            try {
+                System.out.print("\nEnter rows count: ");
+                rowCount = in.nextInt();
+
+                System.out.print("\nEnter columns count: ");
+                columCount = in.nextInt();
+            } catch(Exception e) {}            
+        }
+        while (rowCount == 0 || columCount == 0);
+
+        matrix = new int[rowCount][columCount];
+
+        int i = 0;
+        for (int[] row : matrix)
+        {
+            int j = 0;
+            for (int x : row)
+            {
+                System.out.printf("\ta[%1$d][%2$d]= ", i, j);
+                try {
+                    matrix[i][j] = in.nextInt();
+                } catch(Exception e) {}
+                j++;
+            }
+            System.out.println();
+            i++;
+        } 
+    }
+
+    private void fillMatrixWithRandomValues()
+    {
+        int    rowCount;
+        int    columnCount;
+        Random rand = new Random();
+        do {
+            rowCount    = rand.nextInt(10);
+            columnCount = rand.nextInt(10);
+        } while (rowCount == 0 || columnCount == 0);
+
+        matrix = new int[rowCount][columnCount];
+
+        int i  = 0;
+        for(int[] row : matrix)
+        {
+            int j = 0;
+            for (int x : row) {
+                matrix[i][j] = rand.nextInt(100);
+                j++;
+            }
+            i++;
+        }        
+    }
+
+    private void transformMatrix()
+    {
+        int tmp;
+        
+        // shift to right items in rows
+        for(int i=0; i<matrix.length; i++)
+        {
+            tmp = matrix[i][matrix[i].length-1];
+            for(int j = matrix[i].length - 1; j > 0; j--) {                
+                matrix[i][j] = matrix[i][j-1];
+            }
+            matrix[i][0] = tmp;        
+        }
+
+        // trick: special transform first column
+        tmp = matrix[matrix.length-1][0];
+        for(int i = matrix.length-1; i>0; i--) {
+            matrix[i][0] = matrix[i-1][0];;
+        }
+        matrix[0][0] = tmp;
+    }
+
+    private void run()
     {
         System.out.println("Please choose the working mode:");
         System.out.println("(1) - matrix will be generated automaticaly;");
         System.out.println("(2) - matrix will be entered from keyboard.");
         
         Scanner in = new Scanner(System.in);
+        
         int mode = -1;
         try {
             mode = in.hasNextInt() ? in.nextInt() : -1;
         }
-        catch (Excepton e) {
-            mode = -1;
-        }
+        catch (Exception e) {}
 
-        if (mode != 1 || mode != 2) {
+        if ( ! (mode == 1 || mode == 2)) {
             System.out.println("Unrecognised working mode. Program terminated.");
             return;
         }
-        
-        Task07 t = new Task07();
+
         if (mode == 1) {
-            t.fillMatrixWithRandomValues();            
+            fillMatrixWithRandomValues();            
         }
         else {
-            t.fillMatrixFromKeyboard();
+            fillMatrixFromKeyboard();
         }
 
         System.out.println("Input matrix A:");
-        t.print_r(t.matrix);
+        print_r(matrix);
         
-        t.run(t.matrix);
+        transformMatrix();
         
         System.out.println("Output matrix A:");
-        t.print_r(t.matrix);
-    }
-
-    private void fillMatrixFromKeyboard() {
-
-    }
-
-    private void fillMatrixWithRandomValues() {
-
-    }
-
-    private void run(int[][] matrix) {
-
+        print_r(matrix);
     }
 }
---
+++ src/Task08.java
@@ -0,0 +1,26 @@
+package src;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Find a ten-digit number that all digits are different by rules:
+ * 1) the number, composed of the first 2 digits is divisible by 2;
+ * 2) the number, composed of the first 3 digits is divisible by 3;
+ * 3) the number, composed of the first 4 digits is divisible by 4;
+ * ...
+ * x) and the number itself is divided by 10.
+ */
+
+class Task08 extends AbstractTask {
+
+    public static void main(String[] args) {
+        Task08 t = new Task08();
+        t.run();
+    } 
+
+    private void run() {
+
+    }
+
+}
